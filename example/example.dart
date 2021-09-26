27511169fa93f2ca563c6b9f9edf9096e2bf931d
srthk <techysrthk@gmail.com>
2021-09-26 14:27:48 +0530

    release 2.0.3

--- example/example.dart
+++ example/example.dart
@@ -1,28 +1,28 @@
-// import 'package:splitwise_api/splitwise_api.dart';
-// import 'package:splitwise_api/src/util/data/model/current_user_entity.dart';
-//
-//
-// void main() async {
-//   SplitWiseService splitWiseService =
-//   SplitWiseService.initialize(_consumerKey, _consumerSecret);
-//
-//   /// SplitWiseHelper is for saving and retrieving from shared storage
-//   SplitWiseHelper splitWiseHelper = SplitWiseHelper();
-//   if (splitWiseHelper.getTokens() == null) {
-//     var authURL = splitWiseService.validateClient();
-//     print(authURL);
-//     //This Will print the token and also return them save them to Shared Prefs
-//     TokensHelper tokens = await splitWiseService.validateClient(
-//         verifier: 'theTokenYouGetAfterAuthorization');
-//     await splitWiseHelper.saveTokens(tokens);
-//
-//     splitWiseService.validateClient(tokens: tokens);
-//   } else {
-//     splitWiseService.validateClient(
-//         tokens: /* tokens from saved */);
-//     //Example
-//     CurrentUserEntity currentUserEntity = await splitWiseService
-//         .getCurrentUser();
-//     print(currentUserEntity.user.firstName);
-//   }
-// }
+import 'package:splitwise_api/splitwise_api.dart';
+import 'package:splitwise_api/src/util/data/model/current_user_entity.dart';
+
+
+void main() async {
+  SplitWiseService splitWiseService =
+  SplitWiseService.initialize(_consumerKey, _consumerSecret);
+
+  /// SplitWiseHelper is for saving and retrieving from shared storage
+  SplitWiseHelper splitWiseHelper = SplitWiseHelper();
+  if (splitWiseHelper.getTokens() == null) {
+    var authURL = splitWiseService.validateClient();
+    print(authURL);
+    //This Will print the token and also return them save them to Shared Prefs
+    TokensHelper tokens = await splitWiseService.validateClient(
+        verifier: 'theTokenYouGetAfterAuthorization');
+    await splitWiseHelper.saveTokens(tokens);
+
+    splitWiseService.validateClient(tokens: tokens);
+  } else {
+    splitWiseService.validateClient(
+        tokens: /* tokens from saved */);
+    //Example
+    CurrentUserEntity currentUserEntity = await splitWiseService
+        .getCurrentUser();
+    print(currentUserEntity.user.firstName);
+  }
+}
